b59dc6eb8048a21e090ac626311387ffda00bd0c
vu hiep <vuxhiep97@gmail.com>
2022-05-04 22:23:37 +0700

    add home frontend

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
+});
+
+Route::get('category', function () {
+    return view('home.category');
+});
+
+Route::get('post', function () {
+    return view('home.article');
 });
